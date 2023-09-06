2af1e7951ad689838984e8607abc758e24dd9132
charles <wkc1002@naver.com>
2023-09-06 17:54:31 +0900

    Record Controller도 완성

--- src/main/java/springschool/ranking/record/controller/RecordController.java
+++ src/main/java/springschool/ranking/record/controller/RecordController.java
@@ -5,17 +5,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import springschool.ranking.record.Policy;
 import springschool.ranking.record.service.RecordService;
-import springschool.ranking.student.domain.Student;
+import springschool.ranking.record.service.dto.ScoreInputDto;
+import springschool.ranking.record.service.dto.partial.PartialRecordDto;
+import springschool.ranking.record.service.dto.partial.PartialRecordListDto;
 import springschool.ranking.student.service.StudentService;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/rank")
+@RequestMapping("/api/record")
 public class RecordController {
 
     private final RecordService recordService;
-    private final StudentService studentService;
 
     /**
      * 성적 정책을 선택하는 로직이다.
@@ -37,26 +38,24 @@ public class RecordController {
      * @return 학생 한 명의 석차를 담은 객체를 반환한다.
      */
     @GetMapping("/{studentId}/v1")
-    RankDto checkRankOneV1(@PathVariable Long studentId) {
-
-        int rank = recordService.getRankOne(studentId);
-        Student student = studentService.findStudent(studentId);
-
-        return new RankDto(studentId, student.getName(), rank);
+    PartialRecordDto checkRankOneV1(@PathVariable Long studentId) {
+        return recordService.getRankOne(studentId);
     }
 
     /**
      * @return 전체 학생들의 석차를 담은 객체 리스트를 반환한다.
      */
-    @GetMapping("/rankList/v1")
-    RankListDto studentRankListV1() {
-
+    @GetMapping("/recordList/v1")
+    PartialRecordListDto studentRankListV1() {
         return recordService.sortRank();
     }
 
     /**
      * 해당 학생 점수 입력
+     * 완료 시 학생 성적 조회로 리다이렉트
      */
-    @PostMapping("/{studentId}/inputRank/v1")
-    RankDto studentRankInput
+    @PostMapping("/{studentId}/inputRecord/v1")
+    PartialRecordDto saveRecordV1(@PathVariable Long studentId, @RequestBody ScoreInputDto scoreInputDto) {
+        return checkRankOneV1(studentId);
+    }
 }
